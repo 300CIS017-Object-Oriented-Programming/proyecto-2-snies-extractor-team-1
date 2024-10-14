92a88bbe13b493dc12c191edd4ff76d4b475580a
migueangel1228 <miguelangel1228@javerianacali.edu.co>
2024-10-13 19:44:44 -0500

    12-creación-clase-gestorjson - NEW:creacion de clase y implementacion de GestorJson con su metodo exportar datos

--- src/GestorDatos.h
+++ src/GestorDatos.h
@@ -16,12 +16,14 @@ using std::vector;
 
 class GestorDatos {
 public:
-    // Constructor y Destructor
-    GestorDatos();
-    ~GestorDatos();
-
+    // Destructor
+    virtual ~GestorDatos() = default;
     // Metodo para escribir datos procesados en un archivo
-    virtual void exportarDatos(const std::string& filePath) = 0;
+    virtual void exportarDatos(const string& filePath, const vector<map<string, string>>& datos) = 0;
+
+protected:
+    // Metodo de ayuda para manejar errores comunes
+    void manejarErrores(const string& mensaje);
 };
 
 #endif // GESTOR_DATOS_H
