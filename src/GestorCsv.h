28699f7c4c78f7a1bd8e8de068765751c4047040
Figue4870 <131775845+Figue4870@users.noreply.github.com>
2024-10-23 22:27:46 -0500

    MOD 6: Unificación de métodos para lectura de archivos

--- src/GestorCsv.h
+++ src/GestorCsv.h
@@ -6,6 +6,7 @@
 #include <map>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 #include "programaAcademico.h"
 #include <list>
 #include "Constantes.h"
@@ -22,12 +23,13 @@ public:
     GestorCsv() = default;
     vector<string> leerProgramasCsv(string &ruta);
     // Mantenimiento: Se puede mejorar la firma y nombre de los metodos para que sea más descriptiva
-    vector<vector<string>> leerArchivoPrimera(string &rutaBase, string &ano, vector<int> &codigosSnies);
-    vector<vector<string>> leerArchivoSegunda(string &rutaBase, string &ano, vector<int> &codigosSnies);
+    vector<vector<string>> GestorCsv::leerArchivoUnificado(string &rutaBase, string &ano, vector<int> &codigosSnies, int colmunaCodigoSnies);
     vector<vector<string>> leerArchivo(string &rutaBase, string &ano, vector<int> &codigosSnies, int colmunaCodigoSnies);
+    int contarColumnas(string &rutaBase, string &ano);
     bool crearArchivo(string &ruta, map<int, ProgramaAcademico *> &mapadeProgramasAcademicos, vector<string> etiquetasColumnas);
     bool crearArchivoBuscados(string &ruta, list<ProgramaAcademico *> &programasBuscados, vector<string> etiquetasColumnas);
     bool crearArchivoExtra(string &ruta, vector<vector<string>> datosAImprimir);
+    
 };
 
 #endif
